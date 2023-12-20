55bea1dc71fe3fd1b5f794465b6232dd6e32e587
HussienAboSoker <128934263+HussienAboSoker@users.noreply.github.com>
2023-12-20 02:14:07 +0200

    change them of floating actionbutton

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       // primaryColor:Colors.,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.white ,
+          focusColor: Colors.red[250] ,
+        ),
+        primaryColor:Colors.white,
         textTheme:  TextTheme(
           bodyLarge: const TextStyle(
             fontSize: 22,
